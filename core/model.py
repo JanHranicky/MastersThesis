7aa7c21e673b466bf536fa3566ba7783ebba01e7
JanHranicky <xhrani02@vutbr.cz>
2023-10-13 15:55:16 +0200

    model architecture can be changed using rule_model parameter

--- core/model.py
+++ core/model.py
@@ -5,19 +5,23 @@ class CA(tf.keras.Model):
   '''
   Represents the model of cellular automata
   '''
-  def __init__(self,channel_n=16,cell_reset_prob=0.5,model_name="CA"):
+  def __init__(self,channel_n=16,cell_reset_prob=0.5,model_name="CA",rule_model=None):
     super().__init__() #Need to be called to initialize the super tf.keras.Model class in order to use tensorflow utilities
 
     self.cell_reset_prob = cell_reset_prob
     self.channel_n = channel_n
     self.model_name = model_name
-    self.rule_model = tf.keras.Sequential([
+    self.rule_model = self.set_rule_model(rule_model,channel_n)
+    self(tf.zeros([1,3,3,channel_n])) #dummy call to initialiaze model, the dummy shape does not have to be the same as final data. But it's dimensionality should be similiar
+
+  def set_rule_model(self,type,channel_n):
+    if type is not None:
+      return type
+    return tf.keras.Sequential([
       tf.keras.layers.Conv2D(filters=128,kernel_size=1,activation='relu'),
       tf.keras.layers.Conv2D(filters=channel_n,kernel_size=1,
       kernel_initializer=tf.zeros_initializer)
     ])
-
-    self(tf.zeros([1,3,3,channel_n])) #dummy call to initialiaze model, the dummy shape does not have to be the same as final data. But it's dimensionality should be similiar
 
   @tf.function
   def perceive(self,x):
