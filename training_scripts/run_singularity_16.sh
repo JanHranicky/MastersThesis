f3bab128d47fda49fd67da69cfea818a88a7ee3e
JanHranicky <xhrani02@vutbr.cz>
2023-10-18 19:29:25 +0200

    added training scripts

--- training_scripts/run_singularity_16.sh
+++ training_scripts/run_singularity_16.sh
@@ -4,6 +4,6 @@
 #qsub -l select=1:ncpus=1:mem=10gb:ngpus=1 -q gpu -v c=5 run_singularity.sh
 #qsub -v c=5 run_training.sh
 #qsub -l select=1:ncpus=1:mem=10gb:ngpus=1 -q gpu -l walltime=9:00:00 run_singularity_12.sh 
-DATADIR=/storage/praha1/home/xhrani02/thesis/growing_ca/job_scripts
+DATADIR=/storage/praha1/home/xhrani02/Thesis-main/job_scripts
        
 singularity run --nv /cvmfs/singularity.metacentrum.cz/NGC/TensorFlow\:23.08-tf2-py3.SIF "$DATADIR/run_training_16.sh"
