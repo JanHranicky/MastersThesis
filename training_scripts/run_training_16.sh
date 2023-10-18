b80062aa636dc50e588986f029f7144dbdd5cbe2
Jan Hranicky <xhrani02@tarkil.metacentrum.cz>
2023-10-18 19:21:29 +0200

    added scripts that run training on Metacentrum

---
+++ training_scripts/run_training_16.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#qsub -v c=5 run_training.sh
+DATADIR=/storage/praha1/home/xhrani02/thesis/growing_ca
+cd $DATADIR
+
+python -m pip install matplotlib
+#python --version > out.txt         
+#singularity run --nv /cvmfs/singularity.metacentrum.cz/NGC/TensorFlow\:23.08-tf2-py3.SIF
+python grayscale_training.py 16
