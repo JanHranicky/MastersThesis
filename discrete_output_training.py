c3a2ede85b5bab5ed57024eb28172b9016fdf0f6
JanHranicky <xhrani02@vutbr.cz>
2023-11-13 23:12:03 +0100

    changed default CA for model without "simulation" layers

--- discrete_output_training.py
+++ discrete_output_training.py
@@ -1,4 +1,4 @@
-from core import model,utils,trainer
+from core import model,utils,trainer,added_conv_model
 import tensorflow as tf
 from PIL import Image,ImageOps
 from IPython.display import clear_output,display
@@ -8,7 +8,7 @@ import IPython.display as display
 from matplotlib import pyplot as plt
 
 class discreteOutTrainer(trainer.Trainer):
-    def __init__(self,model,loss_f,gt_img,gt_img_name,grayscale=False,data_pool_training=False,lr=0.001,epoch_num=30000,visualize=True,visualize_iters=100,save_iters=5000,generate_gif_iters=5000,train_step_interval=(75,100)):
+    def __init__(self,model,loss_f,gt_img,gt_img_name,grayscale=False,data_pool_training=False,lr=0.001,epoch_num=300000,visualize=True,visualize_iters=10000,save_iters=5000,generate_gif_iters=5000,train_step_interval=(75,100)):
         super().__init__(model,loss_f,gt_img,gt_img_name,grayscale,data_pool_training,lr,epoch_num,visualize,visualize_iters,save_iters,generate_gif_iters,train_step_interval)
         
     @tf.function
@@ -21,15 +21,16 @@ GT_IMG_PATH = './img/vut_logo_small.png'
 gt_img = Image.open(GT_IMG_PATH)
 gt_img = ImageOps.grayscale(gt_img)
 
-ca = model.CA(channel_n=16,model_name="discrete_output_CA")
+ca = added_conv_model.CA(channel_n=16,model_name="13_11_2023_discrete_output")
 loss_f = tf.keras.losses.MeanSquaredError()
 
 t = discreteOutTrainer(ca,
                     loss_f,gt_img,
                     GT_IMG_PATH.split('/')[-1].split('.')[0],
-                    generate_gif_iters=100,
+                    generate_gif_iters=10000,
                     grayscale=True,
                     data_pool_training=True,
-                    visualize=False
+                    visualize=False,
+                    visualize_iters=10000
                     )
 t.train()
