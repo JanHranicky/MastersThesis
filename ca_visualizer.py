b7de892b63a68039de1b3835a65f1237e82c7096
JanHranicky <xhrani02@vutbr.cz>
2023-11-13 18:55:36 +0100

    changed visualizer paths

--- ca_visualizer.py
+++ ca_visualizer.py
@@ -1,18 +1,20 @@
-from core import model,utils
+from core import model,utils,added_conv_model
 import tensorflow as tf
 from PIL import Image
 from matplotlib import pyplot as plt
 
 STEP_NUM = 1000
 CHANNELS = 16
-CHECKPOINT_PATH = './checkpoints/Meta_persistent/persistent_pattern_ca'+str(CHANNELS)+'_vut_logo_small/100000'
+CHECKPOINT_PATH = './checkpoints/added_conv_layer_ca_vut_logo_small/100000'
+#CHECKPOINT_PATH = './checkpoints/Meta_persistent/persistent_pattern_ca'+str(CHANNELS)+'_vut_logo_small/100000'
 GT_IMG_PATH = './img/vut_logo_small.png'
-    
+GIF_NAME = 'trainable_conv_layer'
+
 width,height = Image.open(GT_IMG_PATH).size
 x = utils.init_batch(1,width,height)
 
-ca = model.CA(channel_n=CHANNELS)
+ca = added_conv_model.CA(channel_n=CHANNELS)
 ca.load_weights(CHECKPOINT_PATH)
 
 imgs = utils.run_model_for_i(ca,width,height,CHANNELS,1000,True)
-utils.make_gif(str(CHANNELS)+'_channels_grayscale_persistent',imgs)
+utils.make_gif(GIF_NAME,imgs)
