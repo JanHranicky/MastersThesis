31925133e5c1e5aa63ed96982ee411d5c2d2d6a7
JanHranicky <xhrani02@vutbr.cz>
2023-11-13 23:28:12 +0100

    added batch norm script

---
+++ batch_norm_training.py
@@ -0,0 +1,53 @@
+from core import model,utils,trainer,added_conv_model
+import tensorflow as tf
+from PIL import Image,ImageOps
+from IPython.display import clear_output,display
+import numpy as np 
+import os
+import IPython.display as display
+from matplotlib import pyplot as plt
+
+class discreteOutTrainer(trainer.Trainer):
+    def __init__(self,model,loss_f,gt_img,gt_img_name,grayscale=False,data_pool_training=False,lr=0.001,epoch_num=300000,visualize=True,visualize_iters=10000,save_iters=5000,generate_gif_iters=5000,train_step_interval=(75,100)):
+        super().__init__(model,loss_f,gt_img,gt_img_name,grayscale,data_pool_training,lr,epoch_num,visualize,visualize_iters,save_iters,generate_gif_iters,train_step_interval)
+        
+    @tf.function
+    def train_step(self,x,trainer):
+      iter_n = tf.random.uniform([], self.train_step_interval[0], self.train_step_interval[1], tf.int32)
+      with tf.GradientTape() as g:
+        for i in tf.range(iter_n):
+          x = self.model(x)
+          if self.grayscale:
+            l_x = utils.tf2grayscale(x)
+          else:
+            l_x = x
+        loss = tf.math.reduce_mean(self.loss_f(self.gt_img, l_x))
+      grads = g.gradient(loss, self.model.weights)
+      trainer.apply_gradients(zip(grads, self.model.weights))
+      return tf.cast(tf.cast(x,dtype=tf.int32),dtype=tf.float32), loss
+
+
+GT_IMG_PATH = './img/vut_logo_small.png'
+gt_img = Image.open(GT_IMG_PATH)
+gt_img = ImageOps.grayscale(gt_img)
+
+
+layers = tf.keras.Sequential([
+      tf.keras.layers.Conv2D(filters=128,kernel_size=1,activation='relu'),
+      tf.keras.layers.BatchNormalization(),
+      tf.keras.layers.Conv2D(filters=16,kernel_size=1,
+      kernel_initializer=tf.zeros_initializer),
+    ])
+ca = added_conv_model.CA(channel_n=16,model_name="13_11_2023_batch_norm",rule_model=layers)
+loss_f = tf.keras.losses.MeanSquaredError()
+
+t = discreteOutTrainer(ca,
+                    loss_f,gt_img,
+                    GT_IMG_PATH.split('/')[-1].split('.')[0],
+                    generate_gif_iters=10000,
+                    grayscale=True,
+                    data_pool_training=True,
+                    visualize=False,
+                    visualize_iters=10000
+                    )
+t.train()
