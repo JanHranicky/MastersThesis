4ea84c21b34054ff3930b016c65eba66c3e3494c
Jan Hranicky <xhrani02@tarkil.metacentrum.cz>
2023-12-02 16:19:26 +0100

    changed path

--- training_scripts/run_singularity.sh
+++ training_scripts/run_singularity.sh
@@ -5,6 +5,6 @@
 #qsub -v c=5 run_training.sh
 #qsub -l select=1:ncpus=1:mem=10gb:ngpus=1 -q gpu -l walltime=9:00:00 run_singularity_12.sh
 #qsub -l select=1:ncpus=1:mem=10gb:ngpus=1 -q gpu -l walltime=9:00:00 -v SCRIPT=PATH run_singularity.sh 
-DATADIR=/storage/praha1/home/xhrani02/Thesis-main/training_scripts
+DATADIR=/storage/praha1/home/xhrani02/GrowingCA/Thesis/training_scripts
        
 singularity run --nv /cvmfs/singularity.metacentrum.cz/NGC/TensorFlow\:23.08-tf2-py3.SIF "$DATADIR/$SCRIPT"
