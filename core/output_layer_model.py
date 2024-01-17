7eb1e5ad7c041526f716338fa37da72babde6890
JanHranicky <xhrani02@vutbr.cz>
2024-01-17 19:40:35 +0100

    fixed model

---
+++ core/output_layer_model.py
@@ -0,0 +1,51 @@
+from core import utils
+import tensorflow as tf
+
+class CA(tf.keras.Model):
+  '''
+  Represents the model of cellular automata
+  '''
+  def __init__(self,channel_n=16,model_name="CA",states=8):
+    super().__init__() #Need to be called to initialize the super tf.keras.Model class in order to use tensorflow utilities
+
+    self.states = states
+    self.channel_n = channel_n
+    self.model_name = model_name
+    self.rule_model = self.set_rule_model(channel_n)
+    self.perceive_conv = tf.keras.layers.DepthwiseConv2D(
+      kernel_size=3,
+      depth_multiplier=3,
+      strides=[1, 1],
+      padding='SAME'
+    )
+    self(tf.zeros([1,3,3,channel_n])) #dummy call to initialiaze model, the dummy shape does not have to be the same as final data. But it's dimensionality should be similiar
+
+
+  def modulo_activation(self, x):
+    #return x
+    mod_x = tf.math.floormod(x,tf.ones_like(x,dtype=tf.float32)*self.states)
+    #return mod_x
+    return my_floor(mod_x)
+
+  
+  def set_rule_model(self,channel_n):
+    return tf.keras.Sequential([
+      tf.keras.layers.Conv2D(filters=128,kernel_size=1,activation='elu'),
+      tf.keras.layers.Conv2D(filters=channel_n,kernel_size=1,
+      kernel_initializer=tf.zeros_initializer, activation=self.modulo_activation),
+    ])
+
+  @tf.function
+  def call(self,x):
+    y = self.perceive_conv(x)
+    dx = self.rule_model(y)
+
+    return dx
+  
+@tf.custom_gradient
+def my_floor(x):
+  def my_floor_grad(dy):
+    return 1*dy
+  f_x = tf.floor(x)
+  return f_x,my_floor_grad
+
---
+++ experiments/output_layer.py
@@ -0,0 +1,119 @@
+from core import model,utils,trainer,output_layer_model
+import tensorflow as tf
+from PIL import Image,ImageOps
+from IPython.display import clear_output,display
+import numpy as np 
+import os
+import IPython.display as display
+from matplotlib import pyplot as plt
+import pathlib
+from datetime import datetime
+import random
+
+class discreteOutTrainer(trainer.Trainer):
+    def __init__(self,model,loss_f,gt_img,gt_img_name,state_num,grayscale=False,data_pool_training=False,lr=0.01,epoch_num=300000,visualize=True,visualize_iters=10000,save_iters=5000,generate_gif_iters=5000,train_step_interval=(75,100)):
+        super().__init__(model,loss_f,gt_img,gt_img_name,grayscale,data_pool_training,lr,epoch_num,visualize,visualize_iters,save_iters,generate_gif_iters,train_step_interval)
+        self.STATE_NUM = state_num
+        self.batch_size = 2
+        self.compare_channels = 1
+        
+        self.gt_tf = utils.img_to_discrete_space_tf(self.gt_img,self.STATE_NUM,self.compare_channels)
+    
+        self.clip_value = 1.0
+    #@tf.function
+    def train_step(self,x,trainer):
+      iter_n = tf.random.uniform([], self.train_step_interval[0], self.train_step_interval[1], tf.int32)
+      with tf.GradientTape() as g:
+        for i in tf.range(iter_n):
+          x = self.model(x)
+        print(x)
+        loss = tf.math.reduce_mean(self.loss_f(x,self.gt_tf,self.STATE_NUM))
+        tf.print('loss:',loss)
+        tf.debugging.check_numerics(loss, "Loss contains NaN or Inf values")
+
+      grads = g.gradient(loss, self.model.weights)
+      tf.print('before clip',grads)
+      grads = [g/(tf.norm(g)+1e-8) for g in grads]
+      tf.print('after clip',grads)
+      trainer.apply_gradients(zip(grads, self.model.weights))
+      
+      return x, loss
+    
+    def train(self):
+      pathlib.Path(self.checkpoint_path).mkdir(parents=True, exist_ok=True) 
+
+      height,width = self.gt_img.size
+      lr_sched = tf.keras.optimizers.schedules.PiecewiseConstantDecay(
+          [2000], [self.lr, self.lr*0.1])
+      trainer = tf.keras.optimizers.Adam(lr_sched)
+      
+      loss_values = []
+      for i in range(self.epoch_num):
+        if self.data_pool_training:
+          x0 = self.dp.get_batch(self.batch_size)
+          #converted = utils.convert_to_comparable_shape(x0,self.compare_channels)
+          highest_loss_i = self.dp.get_highest_loss_index(self.gt_img,x0,self.loss_f,args=(self.gt_tf,self.STATE_NUM))
+          x0 = self.dp.insert_seed_tensor(x0,highest_loss_i)
+        else:
+          x0 = utils.init_batch(self.batch_size,width,height,self.model.channel_n)
+          
+        x, loss = self.train_step(x0,trainer)
+        
+        loss_val = np.log10(loss.numpy())
+        print(f'epoch: {i}, loss_val={loss_val}')
+        loss_values.append(loss_val)
+        
+        if self.data_pool_training:
+          self.dp.commit(x)
+        
+        #self.visualize_batch(tf.math.floormod(x,tf.ones_like(x,dtype=tf.float32)*self.STATE_NUM),i)
+        if np.isnan(loss_val):
+          break
+        self.save_progress(i,loss_values)
+        #self.generate_gif(i,width,height)
+
+GT_IMG_PATH = './img/xhrani02_100x100.png'
+date_time = datetime.now().strftime("%m_%d_%Y")
+gt_img = Image.open(GT_IMG_PATH)
+
+CHANNEL_NUM = 1
+STATES = 8
+
+def custom_mse(x, gt, states):
+    l_x = utils.match_last_channel(x,gt)
+    return tf.reduce_mean(tf.square(l_x - gt))
+
+ca = output_layer_model.CA(channel_n=CHANNEL_NUM,model_name=date_time+'_'+os.path.basename(__file__).split('.')[0]+str(STATES)+"_output_layer",states=STATES)
+#ca.load_weights("./checkpoints/01_10_2024_in_range_single_channel_cmp8_states_single_c_compare_4_channels_xhrani02_100x100/64500")
+
+#loss_f = tf.keras.losses.MeanSquaredError()
+loss_f = custom_mse
+
+t = discreteOutTrainer(ca,
+                    loss_f,gt_img,
+                    GT_IMG_PATH.split('/')[-1].split('.')[0],
+                    state_num=STATES,
+                    generate_gif_iters=10000,
+                    epoch_num=500,
+                    data_pool_training=True,
+                    visualize=False,
+                    visualize_iters=500,
+                    save_iters=500
+                    )
+t.train()
+
+
+#color_list = utils.generate_random_colors(STATES)
+#utils.display_tensor(color_list,t.gt_tf[...,0])
+#height,width = t.gt_img.size
+#x = utils.init_batch(1,width,height,CHANNEL_NUM)
+#
+#for i in range(100):
+#  x = ca(x)
+#  
+#
+#x = tf.math.floormod(tf.math.round(x),tf.ones_like(x,dtype=tf.float32)*STATES)
+#utils.display_tensor(color_list,x[0][:,:,0])
+#tf.print(x,summarize=-1)
+
+
