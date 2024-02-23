c0f2d4b619fbf59df8394aac6da121fd3b125748
JanHranicky <xhrani02@vutbr.cz>
2024-02-23 15:44:13 +0100

    adding channel parameter

--- experiments/differential_evolution_test.py
+++ experiments/differential_evolution_test.py
@@ -13,11 +13,31 @@ import random
 import sys 
 import math
 
+def parse_parameters():
+    # Check if at least one command-line argument is provided
+    if len(sys.argv) < 3:
+        print("Please provide a parameters as a command-line argument.")
+        exit()
+
+    # Get the command-line argument (assuming it's the first one after the script name)
+    parameter_str_channels = sys.argv[1]
+    try:
+        # Convert the parameter to an integer
+        parameter_int = int(parameter_str_channels)
+
+        # Save the integer or use it as needed
+        print("Integer parameter:", parameter_int)
+
+        return parameter_int
+
+    except ValueError:
+        print("Error: The provided parameter is not a valid integer.")
+
 GT_IMG_PATH = './img/vut_logo_17x17_2px_padding.png'
 date_time = datetime.now().strftime("%m_%d_%Y")
 gt_img = Image.open(GT_IMG_PATH)
 
-CHANNEL_NUM = 1
+CHANNEL_NUM = parse_parameters()
 STATES = 8
 STEPS = (20,30)
 
