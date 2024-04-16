90e528f53481d99ba4d1d213a59d8d4a0a30af3e
JanHranicky <xhrani02@vutbr.cz>
2024-04-16 14:03:28 +0200

    full range argument

--- experiments/dnca.py
+++ experiments/dnca.py
@@ -14,12 +14,13 @@ def parse_arguments():
 
     # Add arguments
     parser.add_argument('-c', '--channels', type=int, help='Number of channels of the model', default=2)
-    parser.add_argument('-i', '--iters', type=int, help='Maximum number of iterations', default=1000)
+    parser.add_argument('-i', '--iters', type=int, help='Maximum number of iterations', default=10)
     parser.add_argument('-s', '--states', type=int, help='Size of the state space.', default=8)
     parser.add_argument('-t', '--train_interval', type=utils.parse_int_tuple, help='Train interval of the network', default=(20,30))
     parser.add_argument('-m', '--image', type=str, help='Path to GT image', default='./img/vut_logo_17x17_2px_padding.png')
     parser.add_argument('-r', '--run', type=int, help='Number of the run. If provided results will be stored in a subfolder', default=None)
     parser.add_argument('-f', '--folder', type=str, help='Folder in which the reults will be stored', default='./checkpoints/DNCA/')
+    parser.add_argument('-g', '--full_range', type=bool, help='If set to true will validate all RGB channels of the image', default=False)
 
     return parser.parse_args()
 
@@ -40,7 +41,8 @@ class DncaTrainer():
                generate_gif_iters=5000,
                train_step_interval=(75,100),
                run=None,
-               folder='./checkpoints/'
+               folder='./checkpoints/',
+               full_range=False
                ):
     self.batch_size = batch_size
     self.state_num = state_num
@@ -48,6 +50,7 @@ class DncaTrainer():
     self.model = model
     self.loss_f = loss_f
     
+    self.full_range = full_range and self.model.channel_n >= 3
     
     self.epoch_num = epoch_num
     self.lr = lr
@@ -56,7 +59,11 @@ class DncaTrainer():
     self.gt_img = gt_img
     self.gt_img_name = gt_img_name
     self.height, self.width = self.gt_img.size
-    self.gt_tf = self.img_to_discrete_tensor(gt_img,state_num)
+    
+    if not self.full_range:
+      self.gt_tf = self.img_to_discrete_tensor(gt_img,state_num)
+    else:
+      self.gt_tf = tf.convert_to_tensor(gt_img, dtype=tf.float32)
     
     self.color_dict = {i: (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) for i in range(self.state_num+1)}
     
@@ -115,8 +122,8 @@ class DncaTrainer():
     for i in range(self.epoch_num):
       if self.data_pool_training:
         x0 = self.dp.get_batch(self.batch_size)
-        converted = utils.convert_to_comparable_shape(x0,self.compare_channels)
-        highest_loss_i = self.dp.get_highest_loss_index(self.gt_tf,converted,self.loss_f)
+        #converted = utils.convert_to_comparable_shape(x0,1 if not self.full_range else 3)
+        highest_loss_i = self.dp.get_highest_loss_index(self.gt_tf,x0,self.loss_f)
         x0 = self.dp.insert_seed_tensor(x0,highest_loss_i)
       else:
         x0 = utils.init_batch(self.batch_size,width,height,self.model.channel_n)
@@ -145,6 +152,8 @@ class DncaTrainer():
       safe_name = str(i) if not result else "result_"+str(i)+"_steps"
       self.model.save_weights(self.checkpoint_path+'/'+safe_name)
 
+      np.save(self.checkpoint_path+'/convergence_arr.npy', loss_values)
+      
       plt.plot(loss_values)
       plt.title(f'Loss function epoch num. {i}')
       plt.xlabel('Epoch')
@@ -163,9 +172,14 @@ class DncaTrainer():
     x = utils.init_batch(1,width,height,self.model.channel_n)
     for _ in range(iter):
       x = self.model(x)
-      
-      f = Image.fromarray(np.uint8(x[0][:,:,0].numpy()),mode="L")
-      frames.append(self.grayscale_to_rgb(f))
+
+      if not self.full_range:
+        f = Image.fromarray(np.uint8(x[0][:,:,0].numpy()),mode="L")
+        frames.append(self.grayscale_to_rgb(f))
+      else:
+        f = Image.fromarray(np.uint8(x[0][:,:,:3].numpy()))
+        frames.append(f)
+        
     
     gif_name = str(i) if not result else "result_"+str(iter.numpy())+"_steps"
     self.make_gif(str(self.checkpoint_path)+'/'+gif_name,frames)
@@ -191,7 +205,7 @@ class DncaTrainer():
       
 if __name__ == '__main__':
   arguments = parse_arguments()
-
+  
   date_time = datetime.now().strftime("%m_%d_%Y")
   gt_img = Image.open(arguments.image)
 
@@ -199,7 +213,7 @@ if __name__ == '__main__':
     l_x = utils.match_last_channel(x,gt)
     return tf.reduce_mean(tf.square(l_x - gt))
 
-  ca = output_modulo_model.CA(channel_n=arguments.channels,model_name=date_time+'_modulo_'+os.path.basename(__file__).split('.')[0]+'_'+str(arguments.states)+"_states_"+str(arguments.channels)+"_layers_"+str(arguments.train_interval[0])+"_"+str(arguments.train_interval[1])+"_steps",states=arguments.states)
+  ca = output_modulo_model.CA(channel_n=arguments.channels,model_name=date_time+'_modulo_'+os.path.basename(__file__).split('.')[0]+'_'+str(arguments.states)+"_states_"+str(arguments.channels)+"_layers_"+str(arguments.train_interval[0])+"_"+str(arguments.train_interval[1])+"_steps_full_range_"+str(arguments.full_range),states=arguments.states)
   #ca.load_weights("./checkpoints/01_10_2024_in_range_single_channel_cmp8_states_single_c_compare_4_channels_xhrani02_100x100/64500")
 
   loss_f = custom_mse
@@ -210,14 +224,15 @@ if __name__ == '__main__':
                   gt_img,
                   gt_img.filename.split('/')[-1].split('.')[0],
                   state_num=arguments.states,
-                  generate_gif_iters=2500,
+                  generate_gif_iters=100,
                   data_pool_training=True,
                   visualize_iters=2500,
-                  save_iters=2500,
+                  save_iters=999,
                   train_step_interval=arguments.train_interval,
                   run=arguments.run,
                   folder=arguments.folder,
-                  epoch_num=arguments.iters
+                  epoch_num=arguments.iters,
+                  full_range=arguments.full_range
                   )
   """
     def __init__(self,
@@ -237,6 +252,7 @@ if __name__ == '__main__':
                train_step_interval=(75,100)
                ):
   """
+  
   t.train()
 
 
