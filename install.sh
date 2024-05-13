3f6b315a88963d2617b4529a4222a17bcb1a98eb
JanHranicky <xhrani02@vutbr.cz>
2024-05-13 13:29:13 +0200

    final version of requirements

--- install.sh
+++
@@ -1,8 +0,0 @@
-python -m pip install --upgrade pip setuptools
-python -m pip install tensorflow==2.15.0
-python -m pip install --upgrade Pillow
-python -m pip install matplotlib
-
-
-#python -m pip install --upgrade cython
-#python -m pip install pyjnius
